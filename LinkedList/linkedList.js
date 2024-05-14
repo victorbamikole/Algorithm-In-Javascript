3d3d1e3ba0cefd86f018c4e94313af919198b79c
victorbamikole <victorbamikole92@gmail.com>
2024-05-14 18:19:51 +0100

    singly linkedList

--- LinkedList/linkedList.js
+++ LinkedList/linkedList.js
@@ -1,3 +1,9 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 class LinkedList {
   constructor(value) {
     this.head = {
@@ -32,9 +38,65 @@ class LinkedList {
     this.length++;
     return this;
   }
+
+  insert(index, value) {
+    if (index >= this.length) {
+      return this.append(value);
+    }
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    const leader = this.transverseToIndex(index - 1);
+    const holdingPointer = leader.next;
+    leader.next = newNode;
+    newNode.next = holdingPointer;
+    this.length++;
+    return this.printList();
+  }
+
+  transverseToIndex(index) {
+    //CHECK PARAMS
+    let counter = 0;
+    let currentNode = this.head;
+    while (counter != index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+    console.log(currentNode);
+    return currentNode;
+  }
+
+  remove(index) {
+    //check params
+    const leader = this.transverseToIndex(index - 1);
+    const unwantedNode = leader.next;
+    leader.next = unwantedNode.next;
+    this.length--;
+    return this.printList();
+  }
+
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode != null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+
+    return array;
+  }
 }
 
 const myLinkedList = new LinkedList(10);
 console.log(myLinkedList.append(5));
 console.log(myLinkedList.append(16));
-console.log(myLinkedList.prepend(20));
+console.log(myLinkedList.prepend(1));
+console.log(myLinkedList.insert(2, 99));
+console.log(myLinkedList.insert(20, 88));
+console.log(myLinkedList.remove(2));
+console.log(myLinkedList.remove(2));
+
+// console.log(myLinkedList.printList());
+
+// console.log(myLinkedList.prepend(1));
